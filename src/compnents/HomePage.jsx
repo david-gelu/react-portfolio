44056dd96f3d78d357b432730018388269f304d6
david-gelu <david.gelu90@gmail.com>
2022-01-27 15:15:18 +0200

    minorchanges on home page

--- src/compnents/HomePage.jsx
+++ src/compnents/HomePage.jsx
@@ -2,7 +2,8 @@ const HomePage = () => {
   return (
     <main className="main " id="main">
       <div className="main-name-div left">
-        <h1>I'm <span>Gelu</span><br /> and I <i className="fas fa-book-reader" /> to <i className="far fa-file-code" /></h1>
+        <h1>I'm <span>Gelu</span></h1><br />
+        <h3>junior web developer</h3>
       </div>
     </main>
   )
