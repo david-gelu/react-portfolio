88e33fb664b74c6e65f26d5f2080cd3f71694e47
david-gelu <david.gelu90@gmail.com>
2022-07-06 22:37:45 +0300

    test pwa

--- src/serviceWorkerRegistration.ts
+++ src/serviceWorkerRegistration.ts
@@ -17,7 +17,7 @@ export function register(config?: Config) {
     }
 
     window.addEventListener('load', () => {
-      const swUrl = `${process.env.PUBLIC_URL}/service-worker.ts`;
+      const swUrl = `${process.env.PUBLIC_URL}/dist/service-worker.js`;
 
       if (isLocalhost) {
         checkValidServiceWorker(swUrl, config);
