b8af141cbffa248115c35b4482debd5c095ba807
david-gelu <david.gelu90@gmail.com>
2022-09-06 15:10:12 +0300

    add cache

--- src/service-worker.js
+++ src/service-worker.js
@@ -9,7 +9,6 @@ import { StaleWhileRevalidate } from 'workbox-strategies';
 clientsClaim();
 
 precacheAndRoute(self.__WB_MANIFEST);
-
 const fileExtensionRegexp = new RegExp('/[^/?]+\\.[^/]+$');
 registerRoute(
   ({ request, url }) => {
@@ -44,4 +43,52 @@ self.addEventListener('message', (event) => {
   if (event.data && event.data.type === 'SKIP_WAITING') {
     self.skipWaiting();
   }
+});
+
+const CACHE_NAME = "version-1";
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/static/media/*.*.webp',
+  '/static/js/*.*.chunk.js',
+  '/static/js/main.*.chunk.js',
+
+
+  // for localhost
+  '/images/*.png',
+  '/static/js/bundle.js',
+  '/static/js/0.chunk.js',
+  '/static/js/main.chunk.js',
+  '/manifest.json',
+]
+
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Opened cache');
+
+        return cache.addAll(urlsToCache);
+      })
+  )
+});
+
+
+self.addEventListener('fetch', function (event) {
+
+  event.respondWith(
+
+    caches.match(event.request)
+      .then(function (response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+
+        }
+        return fetch(event.request);
+
+      }
+      )
+  );
 });
