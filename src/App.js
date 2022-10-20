dd94ac133e78dced319e3ffb147985d516db2880
david-gelu <david.gelu90@gmail.com>
2022-10-20 12:07:48 +0300

    addbutton to instal pwa

--- src/App.js
+++ src/App.js
@@ -1,40 +1,43 @@
 
-import React, { Suspense } from "react"
+import React, { Suspense, useEffect } from "react"
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap-icons/font/bootstrap-icons.css';
 import "./assets/style.scss";
+import { Button } from "react-bootstrap";
 const HomePage = React.lazy(() => import('./compnents/HomePage'));
 const NavBar = React.lazy(() => import('./compnents/NavBar'));
 const ProjectsPage = React.lazy(() => import('./compnents/ProjectsPage'));
 const AboutMePage = React.lazy(() => import('./compnents/AboutMePage'));
 
 const App = () => {
-  // let deferredPrompt;
+  let beforeInstallPrompt
+  useEffect(() => {
+    window.addEventListener("beforeinstallprompt", eventHandler, errorHandler)
+  }, [])
 
-  // window.addEventListener('beforeinstallprompt', (e) => {
-  //   e.preventDefault();
-  //   deferredPrompt = e;
-  //   showInstallPromotion();
-  //   console.log(`'beforeinstallprompt' event was fired.`);
-  // })
+  const eventHandler = (event) => {
+    beforeInstallPrompt = event
+  }
 
-  // const btnAdd = async () => {
-  //   hideInstallPromotion();
-  //   deferredPrompt.prompt();
-  //   const { outcome } = await deferredPrompt.userChoice;
-  //   console.log(`User response to the install prompt: ${outcome}`);
-  //   deferredPrompt = null;
-  // }
+  const errorHandler = (e) => { console.log('error: ' + e) }
+
+  const install = () => {
+    if (beforeInstallPrompt) beforeInstallPrompt.prompt()
+  }
+
+  const checkIfInstaled = window.matchMedia('(display-mode: standalone)').matches || window.navigator.standalone === true
+  console.log(checkIfInstaled)
+
   return (
-    <Suspense fallback={<div className="loading">Loading...</div>}>
-      <NavBar />
-      {/* <button style={{ marginLeft: '5rem' }} onClick={btnAdd}>sdfdf</button> */}
-      <HomePage />
+    <Suspense fallback={<div className="loading" > Loading...</div>}>
+      < NavBar />
+      {!checkIfInstaled && <Button className='dark-btn' size='sm' style={{ position: 'fixed', bottom: 0, right: 0 }} onClick={install} > Install PWA </Button>}
+      < HomePage />
       <ProjectsPage />
-      <AboutMePage />
+      < AboutMePage />
     </Suspense>
-  );
+  )
 }
+
 
 export default App;
