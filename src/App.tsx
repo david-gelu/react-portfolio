ceeceb8b4ad9a73742c39108a0e74560b195a006
david-gelu <david.gelu90@gmail.com>
2022-12-24 14:17:14 +0200

    make App .tsx unstead .js

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 
-import React, { Suspense, useEffect } from "react"
+import React, { Suspense } from "react"
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./assets/style.scss";
-import { Button } from "react-bootstrap";
+import { NavBarProvider } from "./context/NavBarContext";
 const HomePage = React.lazy(() => import('./compnents/HomePage'));
 const NavBar = React.lazy(() => import('./compnents/NavBar'));
 const ProjectsPage = React.lazy(() => import('./compnents/ProjectsPage'));
@@ -12,7 +12,9 @@ const AboutMePage = React.lazy(() => import('./compnents/AboutMePage'));
 const App = () => {
   return (
     <Suspense fallback={<div className="loading" > Loading...</div>}>
-      < NavBar />
+      <NavBarProvider>
+        < NavBar />
+      </NavBarProvider>
       < HomePage />
       <ProjectsPage />
       < AboutMePage />
